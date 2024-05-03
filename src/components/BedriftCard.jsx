d3140b1f293d05b78b853fd2889a9fb2cb031c94
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-05-03 13:35:28 +0200

    style @media

--- src/components/BedriftCard.jsx
+++ src/components/BedriftCard.jsx
@@ -7,85 +7,81 @@ export function BedriftCard({ bedrift }) {
       <div className={styles.card}>
         <div className={styles.bedriftDetaljerLeft}>
           {bedrift.aktivitet && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Aktivitet: </div>
               <div>{bedrift.aktivitet}</div>
             </div>
           )}
           {bedrift.isAntallAnsatteRegistrert && (
-            <div>
-              <div className={styles.title}>
-                Har registrert antall ansatte:{' '}
-              </div>
+            <div className={styles.breakPoint}>
+              <div className={styles.title}>Har registrert antall ansatte:</div>
               <div>{bedrift.isAntallAnsatteRegistrert}</div>
             </div>
           )}
           {bedrift.antallAnsatte && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Antall ansatte: </div>
               <div>{bedrift.antallAnsatte}</div>
             </div>
           )}
           {bedrift.forretningsAdresse && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Forretningsadresse: </div>
               <div>{bedrift.forretningsAdresse}</div>
             </div>
           )}
           {bedrift.hjelpeenhetskodeDesc && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Hjelpeenhetskode beskrivelse: </div>
               <div>{bedrift.hjelpeenhetskodeDesc}</div>
             </div>
           )}
           {bedrift.hjelpeenhetskodeCode && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Hjelpeenhetskode: </div>
               <div>{bedrift.hjelpeenhetskodeCode}</div>
             </div>
           )}
           {bedrift.hjemmeside && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Hjemmeside: </div>
               <div>{bedrift.hjemmeside}</div>
             </div>
           )}
           {bedrift.institusjonellSektorDesc && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>
-                Institusjonell sektor beskrivelse:{' '}
+                Institusjonell sektor beskrivelse:
               </div>
               <div>{bedrift.institusjonellSektorDesc}</div>
             </div>
           )}
           {bedrift.institusjonellSektorCode && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Institusjonell sektor kode: </div>
               <div>{bedrift.institusjonellSektorCode}</div>
             </div>
           )}
           {bedrift.naeringskodeDesc && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Næringskode beskrivelse: </div>
               <div>{bedrift.naeringskodeDesc}</div>
             </div>
           )}
           {bedrift.naeringskodeCode && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Næringskode: </div>
               <div>{bedrift.naeringskodeCode}</div>
             </div>
           )}
           {bedrift.organisasjonsformDesc && (
-            <div>
-              <div className={styles.title}>
-                Organisasjonsform beskrivelse:{' '}
-              </div>
+            <div className={styles.breakPoint}>
+              <div className={styles.title}>Organisasjonsform beskrivelse:</div>
               <div>{bedrift.organisasjonsformDesc}</div>
             </div>
           )}
           {bedrift.organisasjonsformCode && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Organisasjonsform kode: </div>
               <div>{bedrift.organisasjonsformCode}</div>
             </div>
@@ -93,68 +89,71 @@ export function BedriftCard({ bedrift }) {
         </div>
         <div className={styles.bedriftDetaljerRight}>
           {bedrift.postadresse && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Postadresse: </div>
               <div>{bedrift.postadresse}</div>
             </div>
           )}
           {bedrift.registrertIForetaksregisteret && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>
-                Registrert i Foretaksregisteret:{' '}
+                Registrert i Foretaksregisteret:
               </div>
               <div>{bedrift.registrertIForetaksregisteret}</div>
             </div>
           )}
           {bedrift.registrertIFrivillighetsregisteret && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>
-                Registrert i Frivillighetsregisteret:{' '}
+                Registrert i Frivillighetsregisteret:
               </div>
               <div>{bedrift.registrertIFrivillighetsregisteret}</div>
             </div>
           )}
           {bedrift.registrertIMvaregisteret && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Registrert i MVA-registeret: </div>
               <div>{bedrift.registrertIMvaregisteret}</div>
             </div>
           )}
           {bedrift.registrertIStiftelsesregisteret && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>
-                Registrert i Stiftelsesregisteret:{' '}
+                Registrert i Stiftelsesregisteret:
               </div>
               <div>{bedrift.registrertIStiftelsesregisteret}</div>
             </div>
           )}
           {bedrift.sisteInnsendteAarsregnskap && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Siste innsendte årsregnskap: </div>
               <div>{bedrift.sisteInnsendteAarsregnskap}</div>
             </div>
           )}
           {bedrift.registreringsDato && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Registreringsdato: </div>
               <div>{bedrift.registreringsDato}</div>
             </div>
           )}
           {bedrift.tvangsavvikletPgaManglendeSlettingDato && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>
-                Tvangsavviklet pga manglende sletting dato:{' '}
+                Tvangsavviklet pga manglende sletting dato:
               </div>
               <div>{bedrift.tvangsavvikletPgaManglendeSlettingDato}</div>
             </div>
           )}
           {bedrift.konkurs && (
-            <div className={bedrift.konkurs === 'Ja' ? styles.attention : ''}>
+            <div
+              className={
+                bedrift.konkurs === 'Ja' ? styles.attention : styles.breakPoint
+              }>
               <div
                 className={
                   bedrift.konkurs === 'Ja' ? styles.attention : styles.title
                 }>
-                Konkurs:{' '}
+                Konkurs:
               </div>
               <div>{bedrift.konkurs}</div>
             </div>
@@ -162,7 +161,9 @@ export function BedriftCard({ bedrift }) {
           {bedrift.underAvvikling && (
             <div
               className={
-                bedrift.underAvvikling === 'Ja' ? styles.attention : ''
+                bedrift.underAvvikling === 'Ja'
+                  ? styles.attention
+                  : styles.breakPoint
               }>
               <div
                 className={
@@ -170,13 +171,13 @@ export function BedriftCard({ bedrift }) {
                     ? styles.attention
                     : styles.title
                 }>
-                Under avvikling:{' '}
+                Under avvikling:
               </div>
               <div>{bedrift.underAvvikling}</div>
             </div>
           )}
           {bedrift.underAvviklingDato && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Under avvikling dato: </div>
               <div>{bedrift.underAvviklingDato}</div>
             </div>
@@ -186,7 +187,7 @@ export function BedriftCard({ bedrift }) {
               className={
                 bedrift.underTvangsavviklingEllerTvangsopplosning === 'Ja'
                   ? styles.attention
-                  : ''
+                  : styles.breakPoint
               }>
               <div
                 className={
@@ -194,43 +195,43 @@ export function BedriftCard({ bedrift }) {
                     ? styles.attention
                     : styles.title
                 }>
-                Under tvangsavvikling eller tvangsopplosning:{' '}
+                Under tvangsavvikling eller tvangsopplosning:
               </div>
               <div>{bedrift.underTvangsavviklingEllerTvangsopplosning}</div>
             </div>
           )}
           {bedrift.vedtektsdato && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Vedtektsdato: </div>
               <div>{bedrift.vedtektsdato}</div>
             </div>
           )}
           {bedrift.vedtektsfestetFormaal && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Vedtektsfestet formål: </div>
               <div>{bedrift.vedtektsfestetFormaal}</div>
             </div>
           )}
           {bedrift.kommune && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Kommune: </div>
               <div>{bedrift.kommune}</div>
             </div>
           )}
           {bedrift.kommuneCode && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Kommune nummer: </div>
               <div>{bedrift.kommuneCode}</div>
             </div>
           )}
           {bedrift.landCode && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Land kode: </div>
               <div>{bedrift.landCode}</div>
             </div>
           )}
           {bedrift.lang && (
-            <div>
+            <div className={styles.breakPoint}>
               <div className={styles.title}>Språk: </div>
               <div>{bedrift.lang}</div>
             </div>
